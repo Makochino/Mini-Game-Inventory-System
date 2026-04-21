ec50ca90f2570c6295fe038d307f08ef9e831437
Max <maksimatsiuk456@gmail.com>
2026-04-21 12:38:04 +0300

    change: add inventory dict

---
+++ inventory.py
@@ -0,0 +1,14 @@
+inventory = {
+    "Iron Sword": {
+        "category": "weapon",
+        "value": 120,
+        "quantity": 1,
+        "rarity": "common"
+    },
+    "Health Potion": {
+        "category": "potion",
+        "value": 30,
+        "quantity": 5,
+        "rarity": "uncommon"
+    }
+}
