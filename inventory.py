d2a887d5aa4c351a8084678830ee36e7dabc599f
Max <maksimatsiuk456@gmail.com>
2026-04-22 21:59:17 +0300

    change: complete show_inventory func

--- inventory.py
+++ inventory.py
@@ -1,14 +1,28 @@
-inventory = {
+player_inventory = {
     "Iron Sword": {
         "category": "weapon",
         "value": 120,
         "quantity": 1,
-        "rarity": "common"
+        "rarity": "common",
     },
     "Health Potion": {
         "category": "potion",
         "value": 30,
         "quantity": 5,
-        "rarity": "uncommon"
-    }
+        "rarity": "uncommon",
+    },
 }
+
+
+def show_inventory(player_inventory):
+    for item, specs in player_inv.items():
+        print(f"Item: {item}\nSpecs:")
+        for spec, value in specs.items():
+            print(f"   {spec}: {value}")
+        print()
+
+
+def add_item(player_inv):
+    
+
+show_inventory(player_inventory)
