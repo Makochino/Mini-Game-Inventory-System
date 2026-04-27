e55e243288d76d6f12aad99d5e1bc5a3f9d0bfda
Max <maksimatsiuk456@gmail.com>
2026-04-27 16:45:38 +0300

    change: add remove_item func

--- inventory.py
+++ inventory.py
@@ -18,30 +18,36 @@ def show_inventory(inventory):
     result = []
     
     for item, specs in inventory.items():
-        result.append(f"Item: {item}")
-        result.append("Specs:")
-        
-        for spec, value in specs.items():
-            result.append(f"   {spec}: {value}")
-    
-        result.append("")
-    
+        if item != None:
+            result.append(f"Item: {item}")
+            result.append("Specs:")
+            
+            for spec, value in specs.items():
+                result.append(f"   {spec}: {value}")
+
+            result.append("")
+        else:
+            result.append("")
     return "\n".join(result)
 
 
 def add_item(inventory, name, category, value, quantity, rarity):
-    if name not in inventory:
-        inventory.update({
-            name: {
-                "category": category,
-                "value": value,
-                "quantity": quantity,
-                "rarity": rarity}
-        })
-        return "Item added successfully"
-    else:
-        return "Error this item already exists try again"
+    inventory.update({
+        name: {
+            "category": category,
+            "value": value,
+            "quantity": quantity,
+            "rarity": rarity}
+    })
+    return f"Item {name} added successfully"
 
+def remove_item(player_inventory, item):
+    if item in player_inventory:
+        player_inventory.pop(item)
+        return f"Item {item} removed successfully"
+    else:
+        return f"Item {item} doesn't contain in your inventory!!!"
 
 print(add_item(player_inventory, "Legendary Bow", "weapon", 450, 1, "Legendary⭐"))
+print(remove_item(player_inventory, "Iron Sword"))
 print(show_inventory(player_inventory))
